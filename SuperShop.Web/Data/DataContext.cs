5ede0236ef60beb88705016b4b5cb435d97c3f7a
Daiane Farias <daiane.farias.contractor@gmail.com>
2023-05-22 14:54:52 +0100

    First controller

---
+++ SuperShop.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Web.Data.Entities;
+using System.Collections.Generic;
+
+namespace SuperShop.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }    
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        
+        
+        }          
+    }
+}
